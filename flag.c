c9e7c8157131649491ec1596d9f03953b4a709d7
alvinli04 <alvin.li.j@gmail.com>
2021-12-14 15:31:36 -0500

    attempt to get params of poly to show up in decompilatoin

--- flag.c
+++ flag.c
@@ -8,10 +8,11 @@ int poly(int x) {
 
 int main () {
 	int n;
+	int flgafu[11] = {70, 76, 65, 71, 123, 68, 65, 87, 69, 73, 125};
 	printf("Enter the password: ");
 	scanf("%d", &n);
-	int flgafu[11] = {70, 76, 65, 71, 123, 68, 65, 87, 69, 73, 125};
-	if (poly(n) == 3) {
+	int p = poly(n);
+	if (p == 3) {
 		for(int i = 0; i < 11; i++){
 			printf("%c", flgafu[i]);	
 		}
