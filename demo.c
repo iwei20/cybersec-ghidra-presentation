6bb5e968e9a6b0fe1958207878f454e0e2f46102
alvinli04 <alvin.li.j@gmail.com>
2021-12-16 15:15:33 -0500

    added to demo file

--- demo.c
+++ demo.c
@@ -2,10 +2,11 @@
 
 int main () {
 	int n;
+	printf("Enter a number: ");
 	scanf("%d", &n);
 	int sum = 0;
 	for (int i = 0; i <= n; ++i) {
 		sum += i;
 	}
-	printf("%d\n", sum);
+	printf("The result is %d\n", sum);
 }
