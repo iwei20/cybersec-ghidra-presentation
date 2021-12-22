61dcaabe467eb7594095f3f7964977e9ea12c84b
alvinli04 <alvin.li.j@gmail.com>
2021-12-22 15:16:32 -0500

    added cstring demo

--- demo.c
+++ demo.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
+int arisum (int n) {
+	int s = 0;
+	for (int i = 0; i <= n; i++) {
+		s += i;
+	}
+	return s;
+}
+
 int main () {
 	int n;
 	printf("Enter a number: ");
 	scanf("%d", &n);
-	int sum = 0;
-	for (int i = 0; i <= n; ++i) {
-		sum += i;
+	printf("The result is %d\n", arisum(n));
+
+	char cstring[5] = "chart";
+	for (int i = 0; i < 5; i += 2) {
+		printf("%c", cstring[i]);
 	}
-	printf("The result is %d\n", sum);
+	printf("\n%s\n", cstring);
 }
