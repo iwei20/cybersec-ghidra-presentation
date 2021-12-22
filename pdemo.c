9b78038d6d2b8a0050f1b545036413094701ead5
alvinli04 <alvin.li.j@gmail.com>
2021-12-22 15:11:26 -0500

    moved sum to outside function

--- pdemo.c
+++ pdemo.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+int arisum (int n) {
+	int s = 0;
+	for (int i = 0; i <= n; i++){
+		s += i;
+	}	
+	return s;
+}
+
 int main () {
 	int n;
 	printf("Enter a number: ");
 	scanf("%d", &n);
-	int sum = 0;
-	for (int i = 0; i <= n; ++i) {
-		sum += i;
-	}
 	if (0 == 1){
-		printf("The result is %d\n", sum);
+		printf("The result is %d\n", arisum(n));
 	} else {
 		printf("Boo!\n");
 	}
